ff26cc32cb838c47dda71658d76a5971fb933080
Nitin Kondhari <nitinkondhari85@gmail.com>
2023-06-24 15:54:08 +0530

    add filteres search and pagination

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,91 +1,85 @@
-const Product = require("../model/productmodel")
-const ErrorHander = require("../utils/errorhander")
-const catchAsynError = require("../middleware/catchAsyncErrors")
-
+const Product = require("../model/productmodel");
+const ErrorHander = require("../utils/errorhander");
+const catchAsynError = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 //create products --Admin
 exports.createproduct = catchAsynError(async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-      const product = await Product.create(req.body)
-
-      res.status(201).json({
-            success: true,
-            product
-      })
-
-})
-
-
-
-
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
 
 //Get All Products
 
-exports.getAllproduct = catchAsynError( async (req, res) => {
+exports.getAllproduct = catchAsynError(async (req, res) => {
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
 
-      const products = await Product.find()
+  res.status(200).json({
+    success: true,
+    products,
+    productCount,
+  });
+});
 
-      res.status(200).json({
-            success: true,
-            products
-      })
-})
+//Get Product Details
 
-//Get All Product
+exports.getProductDetails = catchAsynError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
 
-exports.getProductDetails = catchAsynError( async (req, res, next) => {
+  if (!product) {
+    return next(new ErrorHander("product not found", 404));
+  }
 
-      let product = await Product.findById(req.params.id);
-
-      if (!product) {
-            return next(new ErrorHander("product not found", 404))
-      }
-
-
-      res.status(200).json({
-            success: true,
-            product
-      })
-})
-
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 //Upadate Product
 
-exports.updateProduct = catchAsynError( async (req, res, next) => {
+exports.updateProduct = catchAsynError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
 
-      let product = await Product.findById(req.params.id)
+  if (!product) {
+    return next(new ErrorHander("product not found", 404));
+  }
 
-      if (!product) {
-            return next(new ErrorHander("product not found", 404))
-      }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
-      product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-            useFindAndModify: false
-      })
-
-      res.status(200).json({
-            success: true,
-            product
-      })
-})
-
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 //Delete Product
 
-exports.deleteProduct = catchAsynError( async (req, res, next) => {
+exports.deleteProduct = catchAsynError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
 
-      let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHander("product not found", 404));
+  }
 
-      if (!product) {
-            return next(new ErrorHander("product not found", 404))
-      }
+  product = await Product.findByIdAndRemove(req.params.id);
 
-      product = await Product.findByIdAndRemove(req.params.id);
-
-      res.status(200).json({
-            success: true,
-            message: "Product Delete Successfully"
-      })
-})
+  res.status(200).json({
+    success: true,
+    message: "Product Delete Successfully",
+  });
+});
