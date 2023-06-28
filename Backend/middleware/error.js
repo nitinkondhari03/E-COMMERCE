1f436380d3a3ed0b89975a6f5e5e7e409d5a942f
Nitin Kondhari <nitinkondhari85@gmail.com>
2023-06-28 08:26:31 +0530

    add duplicate error jsonwebtoken err and token expired err

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -10,6 +10,27 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHander(message, 400);
   }
 
+   //Monogoose duplicate key error
+
+   if(err.code==11000){
+      const message=`Duplicate ${Object.keys(err.keyValue)} Entered`
+      err=new ErrorHander(message,400)
+   }
+
+  //Wrong JWT error
+
+  if(err.name=="JsonWebTokenError"){
+    const message=`Json Web Token is invalid, try again`
+    err=new ErrorHander(message,400) 
+  }
+
+  //JWT EXPIRE error
+
+  if(err.name=="TokenExpiredError"){
+    const message=`Json Web Token is Expired, try again`
+    err=new ErrorHander(message,400) 
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
