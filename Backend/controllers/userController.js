c62bc529ac862c002ca989c6065fcf555f160af1
Nitin Kondhari <nitinkondhari85@gmail.com>
2023-12-05 16:08:01 +0530

    send emails

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -86,9 +86,11 @@ exports.forgotPassword = catchAsynError(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  const resetPasswordUrl = `https://ecommerce-ruby-ten.vercel.app/password/reset/${resetToken}`;
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "ecommerce-ruby-ten.vercel.app"
+  )}/password/reset/${resetToken}`;
 
-  const message = `Your password reset token is :-\n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
 
   try {
     await sendEmail({
